aa22ffa632e2e13905d30bd55bbbae8c5224fcfc
Dada Authority <dadaauthourity23@gmail.com>
2023-05-21 16:26:49 +0100

    Python More Data Structures

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best_key = ""
+    prev = 0
+    if not a_dictionary:
+        return
+    for key in a_dictionary:
+        score = a_dictionary[key]
+        if score > prev:
+            best_key = key
+        prev = score
+    return best_key
+
+
+# a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+# best_key = best_score(a_dictionary)
+# print("Best score: {}".format(best_key))
+
+# best_key = best_score(None)
+# print("Best score: {}".format(best_key))
