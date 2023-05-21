77e078f0d0894ae12091d40e29c2dcc5587e8690
Dada Authority <dadaauthourity23@gmail.com>
2023-05-21 20:48:10 +0100

    task 15 => Delete by value

--- 0x04-python-more_data_structures/test.py
+++ 0x04-python-more_data_structures/test.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-best_score = __import__('10-best_score').best_score
+complex_delete = __import__('102-complex_delete').complex_delete
+print_sorted_dictionary = \
+            __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-my_dict = {'John': 16, 'Alex': 12, 'Bob': 8, 'Mike': 14, 'Molly': 14}
-best_key = best_score(my_dict)
-print("Best: {}".format(best_key))
+a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
+new_dict = complex_delete(a_dictionary, 'C')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+print("--")
+print("--")
+new_dict = complex_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
