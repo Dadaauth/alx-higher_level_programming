464a69021923e38509787ab322ed080b7ab3e59a
Dada Authority <dadaauthourity23@gmail.com>
2023-05-30 01:19:17 +0100

    task 7 correction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,7 +7,10 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as err:
-        print("Exception: ", err, file=sys.stderr)
+        print("Exception:", err, file=sys.stderr)
+        return False
+    except TypeError as err:
+        print("Exception:", err, file=sys.stderr)
         return False
 
 
