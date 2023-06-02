a287c760adc684e00acecdbe981081cd3a646266
Dada Authority <dadaauthourity23@gmail.com>
2023-06-02 19:41:12 +0100

    task 1 => Suare with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A module created for Squares
+
+Classes:
+    Square
+"""
+
+
+def Square:
+    """
+    A Square Class
+
+    Attributes:
+        __size: the size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
