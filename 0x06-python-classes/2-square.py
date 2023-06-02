694d60ebd10907bccc3d2489457592f706b29c1b
Dada Authority <dadaauthourity23@gmail.com>
2023-06-02 20:33:56 +0100

    added some documentations

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """
 A square module for defining squares
-created for alxse project completion
-
-Classes:
-    Square
+Not just squares
+    I have to work even harder
 """
 
 
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,8 +4,19 @@ Module Documentation
 Not Just words
     I love to work hard
 """
+
+
 class Square:
+    """
+    We are talking about the Square class
+        Methods:
+            __init__: initializes the instances created
+            area: returns the area of the square
+    """
     def __init__(self, size=0):
+        """
+        __init__ method: to initialize
+        """
         if isinstance(size, int):
             if size < 0:
                 raise ValueError("size must be >= 0")
@@ -15,5 +26,7 @@ class Square:
             raise TypeError("size must be an integer")
 
     def area(self):
+        """
+        returns the area of the square object
+        """
         return self.__size * self.__size
-    
