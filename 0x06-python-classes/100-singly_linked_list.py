1ef52a793b31a85ad7e74193ffb4d262ce679f8f
Dada Authority <dadaauthourity23@gmail.com>
2023-06-04 22:38:17 +0100

    task 7 -=>> Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-
+"""
+a module for creating a singly linked list data structure
+"""
 
 
 class Node:
+    """
+    A class for each node in the structure
+    """
     def __init__(self, data, next_node=None):
         if not isinstance(data, int):
             raise TypeError("data must be an integer")
         else:
             self.__data = data
-        if next_node != None and not isinstance(next_node, Node):
+        if next_node is not None and not isinstance(next_node, Node):
             raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = next_node
@@ -30,19 +35,25 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if value != None and not isinstance(value, Node):
+        if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = value
 
 
 class SinglyLinkedList:
+    """
+    a class to manipulate the linked list
+        Capabilities:
+            + insert into the linked list
+            + represent the linked list in a string format when printed
+    """
     def __init__(self):
         self.__head = None
 
     def sorted_insert(self, value):
         new = Node(value, None)
-        if self.__head == None:
+        if self.__head is None:
             self.__head = new
         else:
             tmp = self.__head
@@ -50,7 +61,7 @@ class SinglyLinkedList:
                 new.next_node = tmp
                 self.__head = new
             else:
-                while tmp.next_node != None:
+                while tmp.next_node is not None:
                     if value < tmp.next_node.data:
                         break
                     tmp = tmp.next_node
@@ -60,7 +71,7 @@ class SinglyLinkedList:
     def __repr__(self):
         tmp = self.__head
         val_str = ""
-        while tmp != None:
+        while tmp is not None:
             val_str += f"{tmp.data}\n"
             tmp = tmp.next_node
         val_str = val_str[:-1]
