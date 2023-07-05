222de9c3d78e7c18eb8208b9edaaa98e7e8265fa
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-05 11:45:00 +0100

    first task done

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+"""
+A module for working with adding integers
+This module works for addition of two integers
+It also has error predictions and handling
+
+it has only one function
+hoping to add other functions though
+"""
+
+
+def add_integer(a, b=98):
+    """
+    add_integer function ::
+    ::
+        works by adding two integers together,
+        has error handling funcionalities also
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
