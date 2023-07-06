1be111f87a139fafe3da0fd8c397745255b5dbf3
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-06 15:20:07 +0100

    completed last mandatory task

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([...])
+"""
+import unittest
+max_integer = __import__("6-max_integer").max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_values(self):
+        self.assertEqual(max_integer([1, 4, 5, 7, -10]), 7)
+        self.assertEqual(max_integer(), None)
+        self.assertEqual(max_integer([0, 0, 0, 19, 2000000, -100]), 2000000)
+        self.assertEqual(max_integer([True, False]), True)
+        self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([-12]), -12)
