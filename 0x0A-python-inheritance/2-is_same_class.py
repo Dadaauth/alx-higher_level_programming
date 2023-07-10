3f7b34e9c65a410ebeb1350287a7474db4efda19
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-10 14:32:30 +0100

    done third task

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Another version of isinstanceof
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    checks if an object is exactly an instance of
+    a specified class
+    """
+    return obj is a_class()
