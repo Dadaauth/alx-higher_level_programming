66d7111f922998189842910037a4ca15519ce2a4
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-10 15:12:48 +0100

    made some changes

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,6 @@ def inherits_from(obj, a_class):
     compares an objects class and the specified class
     checks for subclases
     """
+    if type(obj) == a_class:
+        return False
     return issubclass(type(obj), a_class)
