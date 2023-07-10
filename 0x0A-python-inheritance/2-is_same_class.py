d3fbb11498e70141953f51f7d22eb023e608307e
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-10 14:39:36 +0100

    upgraded function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,4 +9,12 @@ def is_same_class(obj, a_class):
     checks if an object is exactly an instance of
     a specified class
     """
-    return obj is a_class()
+    if a_class == object:
+        newInstance = a_class
+    else:
+        newInstance = a_class(obj)
+    return obj == newInstance
+
+
+a = [3, 4, 5]
+print(is_same_class(a, object))
