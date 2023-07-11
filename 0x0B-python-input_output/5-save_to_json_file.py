f7a4c94f94e39009d951a76a2dee7dc0d1507ae2
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-11 20:34:36 +0100

    done sixth task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+"""
+a module for manipulating json and files
+"""
+import json
 
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
+
+
+# save_to_json_file({'name': 'clement', 'age': 24}, 'fromJson.txt')
