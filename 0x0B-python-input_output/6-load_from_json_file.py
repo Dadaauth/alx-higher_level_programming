46f23d1a2ace51d24e555dddb24afa24d78dadba
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-11 20:40:04 +0100

    done seventh task

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 """
+creates an object from a json file
 """
+import json
+
+
+def load_from_json_file(filename):
+    """
+    loads an object form a file containing json
+    """
+    with open(filename, 'r') as f:
+        return json.loads(f.read())
+
+
+# print(load_from_json_file("fromJson.txt"))
