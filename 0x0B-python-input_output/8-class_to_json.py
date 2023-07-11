b6712c1c71c0971bffae22e132bff6da425bf054
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-11 21:09:17 +0100

    added documentation to fu nction

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-returns the dict
+returns the dict that works the magic
 """
 import json
 
 
 def class_to_json(obj):
     """
-    returns the dict
+    returns the dict that works the magic
     """
     return obj.__dict__
