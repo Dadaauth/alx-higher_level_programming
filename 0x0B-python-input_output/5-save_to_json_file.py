1e5d3c83c2eff6e13ca0f4c4cec709e4ec4aa0fb
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-11 20:36:24 +0100

    added documentation for function

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,10 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    a function that save a python object to a file
+    as string
+    """
     with open(filename, 'w') as f:
         f.write(json.dumps(my_obj))
 
