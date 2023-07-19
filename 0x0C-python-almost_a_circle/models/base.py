9711007e9ccfee4316b5b3aad405b834d54f637d
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-19 14:20:05 +0100

    done first and second task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A base module in a package
+"""
+
+
+class Base:
+    """
+    A base class for the start of the project
+    python almost a circle
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
