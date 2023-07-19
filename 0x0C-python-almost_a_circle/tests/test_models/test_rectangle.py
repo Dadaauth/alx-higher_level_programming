7554f1754cea2143abf773113863deadf1a6a03b
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-19 18:35:09 +0100

    done fifth task

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -15,7 +15,27 @@ class TestRectangleClass(unittest.TestCase):
         self.r2 = Rectangle(2, 10)
         self.r3 = Rectangle(10, 2, 0, 0, 12)
 
-    def test_ids(self):
+    def tearDown(self):
+        del self.r1
+        del self.r2
+        del self.r3
+
+    def test_a_ids(self):
         self.assertEqual(self.r1.id, 1)
         self.assertEqual(self.r2.id, 2)
-        self.assertequal(self.r3.id, 12)
+        self.assertEqual(self.r3.id, 12)
+
+    def test_errors(self):
+        self.assertRaises(TypeError, Rectangle, ("11", 12, 3, 4))
+        self.assertRaises(TypeError, Rectangle, (11, "23", 3, 4))
+        self.assertRaises(TypeError, Rectangle, (11, 3, {1, 3}, 4))
+        self.assertRaises(TypeError, Rectangle, (11, 12, 3, "dd"))
+
+    def test_v_error(self):
+        self.assertRaises(ValueError, Rectangle, -1, 2, 3, 3)
+        self.assertRaises(ValueError, Rectangle, 1, -2, 3, 3)
+        self.assertRaises(ValueError, Rectangle, 1, 2, -3, 3)
+        self.assertRaises(ValueError, Rectangle, 1, 2, 3, -3)
+
+    def test_area(self):
+        self.assertEqual(self.r2.area(), 20)
