ee0010bdfb86ea645a9abb9759da51885848979d
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-19 15:09:58 +0100

    fixed documentation issue

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
+"""this is a module with only the Rectangle class
+It is a module created for my project
 """
-A rectangle module for python
-almost a circle project
-"""
+
 import Base from base
 
 
 class Rectangle(Base):
-    """
-    a rectangle class that inherits from the
-    Base class
+    """A rectabgle class for this module
     """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        init function documentation
+        """
         self.__width = width
         self.__height = height
         self.__x = x
@@ -20,32 +21,56 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        a width property or attribute
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        the width propertry setter
+        """
         self.__width = value
 
     @property
     def height(self):
+        """
+        an height property or attribute
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        an height property setter
+        """
         self.__height = value
 
     @property
     def x(self):
+        """
+        an x property or attribute
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        an x property setter
+        """
         self.__x = value
 
     @property
     def y(self):
+        """
+        a y property or atribute
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        a y property setter 
+        """
         self.__y = value
