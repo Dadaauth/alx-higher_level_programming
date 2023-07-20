19a93acda8d2009c409bd9241a8e457c83a3d9e9
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-07-20 10:13:40 +0100

    added documentations to the Square class methods

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,10 +16,16 @@ class Square(Rectangle):
             since it has a lot to inherit from the Rectangle class
     """
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes the Square class with the Rectangle class
+        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """
+        The size property or attribute for the Square class
+        """
         return self.width
 
     @size.setter
@@ -32,23 +38,32 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
+        """
+        update method to update the attributes of the Square instance
+        It overides the update method from the parent class [Rectangle]
+        """
         if len(args) == 0 or args is None:
             for key, value in kwargs.items():
                 if key == 'size':
                     setattr(self, 'height', value)
                     setattr(self, 'width', value)
-                else
+                else:
                     setattr(self, key, value)
             return
+
         l_order = ['id', 'size', 'x', 'y']
         idx = 0
         for arg in args:
             if l_order[idx] == 'size':
                 setattr(self, 'height', arg)
                 setattr(self, 'width', arg)
-            else
+            else:
                 setattr(self, l_order[idx], arg)
             idx += 1
 
     def __str__(self):
+        """
+        changes the string representation of the Square class
+        Overides this method from the parent class [Rectangle]
+        """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
