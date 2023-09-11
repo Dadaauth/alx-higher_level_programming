5faa0221f18ab1d1303450c99f4d2229d3ad08a4
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-09-11 21:20:41 +0100

    second task

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVarOne = 'C is fun';
+const myVarTwo = 'Python is cool';
+const myVarThree = 'JavaScript is amazing';
+console.log(myVarOne);
+console.log(myVarTwo);
+console.log(myVarThree);
