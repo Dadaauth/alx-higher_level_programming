e14dc7d5419a9e5898b9a80fa82b3fb6f4185b89
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-09-12 01:36:56 +0100

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const str = parseInt(process.argv[2]);
+let print = '';
+if (isNaN(str)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < str; i++) {
+    for (let y = 0; y < str; y++) {
+      print += 'X';
+    }
+    console.log(print);
+    print = '';
+  }
+}
