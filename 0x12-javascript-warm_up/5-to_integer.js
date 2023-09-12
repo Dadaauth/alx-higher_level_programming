50684a7ad83aa1db5a704a82cd4cb0e9a73e095b
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-09-12 01:02:16 +0100

    fixed semistandard error

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-str = process.argv[2];
+const str = process.argv[2];
 if (isNaN(parseInt(str))) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
   console.log(`My number: ${parseInt(str)}`);
 }
