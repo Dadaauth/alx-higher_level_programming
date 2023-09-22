66bfe374ba2e8c8757b8a024b31e0bf672eb455f
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-09-22 18:59:27 +0100

    I Wrote a MySQL script to display the privileges of two users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Show grants for a particular user in our database
+-- Grants for user 'user_0d_1'
+SHOW GRANTS FOR user_0d_1@localhost;
+-- Grants for user 'user_0d_2'
+SHOW GRANTS FOR user_0d_2@localhost;
