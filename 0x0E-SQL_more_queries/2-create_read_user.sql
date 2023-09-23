162e5dee7b1a24cf74a870b577b38a75677ffeca
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-09-23 07:52:39 +0100

    Recovered vim file and deleted .swp file

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- It also grants the new user 'user_0d_2' with SELECT privilege alone.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- CREATE a new user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0D_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- GRANT only the SELECT privilege to the user
-GRANT SELECT ON hbtn_0d_2.* to 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON hbtn_0d_2.* to 'user_0d_2'@'localhost';
