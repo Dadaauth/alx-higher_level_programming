c665cccbb4e84f5992dae35440d365011b18a1eb
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-11-26 16:51:50 +0100

    ALX ORM PROJECT:     Task2: Done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This is a module documentation for my file
+"""
+import MySQLdb
+import sys
+
+
+def sql_script() -> None:
+    """
+    sql_script function:
+            A wrapper function tobe called when the file is executed
+            from the command line
+    :return: Nothing
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         password=password, db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name = '{}' "
+                "ORDER BY states.id".format(state_name))
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    sql_script()
--- 0x0F-python-object_relational_mapping/rum.sh
+++ 0x0F-python-object_relational_mapping/rum.sh
@@ -1,1 +1,2 @@
-./$1 clement 4141Clement? hbtn_0e_0_usa
+chmod u+x *.py
+./$1 clement 4141Clement? hbtn_0e_0_usa 'Arizona'
