37dcc251ca335d52172bb9265599948de09c630f
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-11-26 18:24:51 +0100

    ALX ORM PROJECT:     Task 6: Added documentation for State class in the file [model_state.py]

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    Documentaion for State class
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
