6ab4dc82aa4d2d55a2ae8d413485d68cfdf90bf6
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-11-26 18:23:26 +0100

    ALX ORM PROJECT:     Task 6: Done

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a module documentation for my file
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
