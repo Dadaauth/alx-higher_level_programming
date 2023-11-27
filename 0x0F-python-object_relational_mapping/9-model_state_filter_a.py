262813a7ddc6dfbdb0856d923fcd086f9e8db50d
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-11-27 13:54:29 +0100

    ALX ORM PROJECT:     Task 9: Done

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -4,7 +4,7 @@ This is a module documentation for my file
 """
 
 import sys
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, text
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
@@ -19,11 +19,7 @@ if __name__ == '__main__':
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     with Session() as session:
-        stmt = select(State)
-        result = session.scalars(stmt).first()
-        # OR USE result = session.scalar(stmt)
-        if result is not None:
+        stmt = select(State).where(text("BINARY name LIKE '%a%'"))
+        for result in session.scalars(stmt):
             print("{}: {}".format(result.id, result.name))
-        else:
-            print("Nothing")
     Base.metadata.create_all(engine)
