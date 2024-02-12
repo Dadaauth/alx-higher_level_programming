97709ff54444a3446a96dc0a9960f1f29e6fe782
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-02-12 15:11:56 +0100

    made script 4-header.sh run in silent mode

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET requwst to a URL and assigns a header variable to the request. It displays the body of the response
-curl -H "X-School-User-Id: 98" $1
+curl -sH "X-School-User-Id: 98" $1
