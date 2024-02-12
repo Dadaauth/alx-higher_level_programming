1e4de6772bf86b6600d22a922fbc0743ff971711
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-02-12 14:43:04 +0100

    rewrote task #1 code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to a URL and display the body of the response
-curl -sL -w "%{http_code}" $1 -o - | awk '/^200$/{p=1}p'
+curl -sL -w "%{http_code}" -o /dev/null $1 | grep -q 200 && curl -sL $1
