afacb2eecd14003576402c6aa427b2de79241767
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-02-13 17:41:17 +0100

    documentation done

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-"""
-Takes a letter and sends a POST request to a URL.
+"""Takes a letter and sends a POST request to a URL.
     We are using .get on the dictionary objects to avoid
 getting an error if the attribute is not found
 """
 import requests
 import requests.exceptions
+import sys
 
 
 def search(letter):
     """My search function searching a Search API"""
     r = requests.post('http://0.0.0.0:5000/search_user', data={'q': letter})
     try:
+        """here is a documentation"""
         json_res = r.json()
         if len(json_res) < 1:
             return 'No result'
         else:
             return f'[{json_res.get('id')}] {json_res.get('name')}'
     except requests.exceptions.JSONDecodeError:
+        """here is a documentation"""
         return 'Not a valid JSON'
 
 
 if __name__ == '__main__':
-    import sys
     try:
+        """here is a documentation"""
         letter = sys.argv[1]
     except IndexError:
+        """here is a documentation"""
         letter = ""
     print(search(letter))
