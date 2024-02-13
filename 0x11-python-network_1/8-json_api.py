eef135ee111022471d19415813f3df38dc19a6b8
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-02-13 17:35:26 +0100

    hehe

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,13 +6,14 @@ import requests.exceptions
 
 
 def search(letter):
+    """My search function searching a Search API"""
     r = requests.post('http://0.0.0.0:5000/search_user', data={'q': letter})
     try:
         json_res = r.json()
         if len(json_res) < 1:
             return 'No result'
         else:
-            return f'[{json_res.id}] {json_res.name}'
+            return f'[{json_res.get('id')}] {json_res.get('name')}'
     except requests.exceptions.JSONDecodeError:
         return 'Not a valid JSON'
 
