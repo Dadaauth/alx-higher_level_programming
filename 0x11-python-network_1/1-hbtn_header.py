43c2ebb2fb6e6217e10b832966f57726e78a8ced
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-02-13 15:36:24 +0100

    task #1 done

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+My module documentation. A docstring"""
+
+import urllib.request
+
+def fetch_header(url):
+    with urllib.request.urlopen(url) as response:
+        print(response.info().get('X-Request-Id'))
+
+if __name__ == '__main__':
+    import sys
+    url = sys.argv[1]
+    fetch_header(url)
