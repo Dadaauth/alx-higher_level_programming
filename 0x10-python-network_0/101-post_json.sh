36b4ed525ffc82aa8398875568020dc66b200a2b
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-02-13 07:57:15 +0100

    here

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request with a file contents
-curl -sL -X POST -d $(cat $2) @($2) -H "Content-Type: application/json" $1
+curl -sL -X POST -d @$2 -H "Content-Type: application/json" $1
