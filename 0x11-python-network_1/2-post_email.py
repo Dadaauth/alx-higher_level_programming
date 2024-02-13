e9dae077c64eca9d2f571607437e948fcab72e10
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-02-13 15:48:05 +0100

    task #2 done

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# sends a POST request to a passed URL with a parameter
+import urllib
+import sys
+
+
+def post_email(url, email):
+    data = {
+        'email': email
+    }
+    data = urllib.parse.urlencode(data)
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        res = response.read()
+        print(res.decode('utf8'))
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    post_email(url, email)
